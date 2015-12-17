c3c850e5c312ee92d57066f847f8268cd70071f2
jordan <jordan.davidson@domo.com>
2015-12-16 22:01:41 -0700

    format tonic example

--- tonicExample.js
+++ tonicExample.js
@@ -8,18 +8,18 @@ server.on('connection', function(connection) {
   names[connection.id] = 'someone';
 });
 
-server.on('message','name', function(context) {
+server.on('message', 'name', function(context) {
   console.log(context.message.first + ' sent me their name');
   names[context.connection.id] = context.message.first;
 });
 
 server.on('request', 'name', function(context) {
   console.log(names[context.connection.id] + ' is asking for my name');
-  context.reply({ first : 'steve', last : 'johnson' });
+  context.reply({ first: 'steve', last: 'johnson' });
 });
 
-server.listen({ port : 1234 }, function(err) {
-  if(err) {
+server.listen({ port: 1234 }, function(err) {
+  if (err) {
     console.log('problem starting the server' + err);
   }
 });
@@ -28,12 +28,12 @@ var Client = require('wyre').Client;
 var client = new Client();
 
 client.connect('ws://localhost:1234', function(err, connection) {
-  connection.send({ type : 'name', first : 'bob', last : 'swenson' }, function(err) {
-    if(err) {
-      console.log('somthing went wrong ' +  err);
+  connection.send({ type: 'name', first: 'bob', last: 'swenson' }, function(err) {
+    if (err) {
+      console.log('somthing went wrong ' + err);
     } else {
-      connection.request({ type : 'name' }, function(err, replyContext) {
-        console.log('the servers name is ' +  replyContext.message.first);
+      connection.request({ type: 'name' }, function(err, replyContext) {
+        console.log('the servers name is ' + replyContext.message.first);
         connection.close();
         server.close();
       });
